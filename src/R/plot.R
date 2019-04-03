123408d6e86eacf4241c0d568644b39aed4ec293
Aditya Visvanathan <visvanathannaditya@gmail.com>
2019-04-03 20:15:03 +0530

    R to html plots

--- src/R/plot.R
+++ src/R/plot.R
@@ -1,3 +1,11 @@
 jpeg ("rplot.jpg", width = 350, heigh = 350)
-plot (AirPassengers)
+metricfile = "METRICFILE"
+rawdata = read.csv (metricfile)
+
+metricdata = rawdata$'COUNTRY'
+metricdata = metricdata[!is.na(metricdata)]
+print(metricdata)
+metricdatats = ts (metricdata,frequency=1,start=c(1970,1))
+
+plot.ts(metricdatats,xlab="Time",ylab="METRIC")
 dev.off()
--- src/app.py
+++ src/app.py
@@ -8,18 +8,26 @@ pathprefix = "/home/canopy/college/sem-6/lab/dwdm-lab/project"
 
 files = glob.glob(pathprefix+"/dwdm-dataset/transpose/*.csv")
 
-metrics = ["Female Population(%)","Agricultural Land(%)","Male Population(%)",
-        "Femaile Population","Total Population","Acces to Electricity(%)",
-        "Male Population","Agricultural Land Area","Infant Mortality Rate",
-        "National Income","GDP","Agricultural value(% of GDP)","Population Density",
+metrics = ["Female Population(%)","Agricultural Land(%)",
+        "Male Population(%)","Femaile Population","Total Population",
+        "Acces to Electricity(%)","Male Population",
+        "Agricultural Land Area","Infant Mortality Rate","National Income",
+        "GDP","Agricultural value(% of GDP)","Population Density",
         "Population age 15-64(%)","CO2 Emission"]
 
 metric_file_map = dict (zip (metrics,files))
 
 app = Flask (__name__)
 app.secret_key = os.urandom (12)
+app.config["CACHE_TYPE"] = "null"
 country =' '
 metric = ' '
+
+@app.after_request
+def add_header (response):
+    response.headers['Cache-Control'] = 'public,max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    return response
 
 @app.route ('/')
 def home ():
@@ -29,6 +37,7 @@ def home ():
     f = open ("../dwdm-dataset/transpose/gdp-transpose.csv","r")
     reader = csv.reader (f,delimiter=',')
     cols = next (reader)
+    cols = cols [1:]
     f.close ()
     return render_template ("index.html",countries = cols, m = metrics)
 
@@ -36,6 +45,7 @@ def home ():
 def showdata():
     selected_c = request.form.get('country')
     selected_m = request.form.get('metric')
+    selected_a = request.form.get('d_action')
     years_list = [] 
     values_list = []
     infile = metric_file_map[selected_m]
@@ -44,12 +54,18 @@ def showdata():
     for row in readr:
         values_list.append (row[str(selected_c)])
         years_list.append (row[' Name'])
-    print (values_list)
+    f.close ()
     finaldata = []
     for x,y in zip (years_list,values_list):
         finaldata.append ([x,y])
-
-    return render_template("showdata.html", data=finaldata,metric=selected_m)
+    if selected_a == 'view':
+        return render_template("showdata.html", data=finaldata,
+                metric=selected_m,country=selected_c)
+    elif selected_a == 'plot':
+        print (metric_file_map[selected_m]+","+selected_c+","+selected_m)
+        proc = subprocess.call(['./shell/plot',metric_file_map[selected_m],
+                selected_c,selected_m])
+        return render_template("showplot.html",metric=selected_m,country=selected_c)
  
 if __name__ == "__main__":
     app.run (debug=True)
